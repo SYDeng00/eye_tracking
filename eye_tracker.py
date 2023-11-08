0623c7059fa994e82ab1b9376d4d2b04c70001f6
Xinyi Liu <s232258@dtu.dk>
2023-11-08 17:08:30 +0100

    reorganize folder structure

--- eye_tracker.py
+++ eye_tracker.py
@@ -69,7 +69,7 @@ def draw_images(slide_number, graph, reverse):
             indices = range(slide_number, slide_number + 2)
             
     for i, loc in zip(indices, [ (290, 1140), (1570, 1140)]):  # Only loop twice and adjust locations
-        image = Image.open(f"./Data/Advertisement/{i}.PNG")
+        image = Image.open(f"./data/Advertisements/{i}.PNG")
         image = resize_image_to_half_screen(image)
         bio = io.BytesIO()
         image.save(bio, format="PNG")
@@ -138,7 +138,7 @@ while pairs:
         df["gaze_right_eye_x"] = [x[0] for x in df["gaze_right_eye"].values]
         df["gaze_right_eye_y"] = [x[1] for x in df["gaze_right_eye"].values]
 
-        file_path = f"./Data/{slide_number}/{participant_id}-data-{str(reverse)}.csv"
+        file_path = f"./data/images_{slide_number}_{slide_number+1}/participant_{participant_id}_reverse_{str(reverse)}.csv"
         directory = os.path.dirname(file_path)
         
         if not os.path.exists(directory):
@@ -148,16 +148,10 @@ while pairs:
             os.remove(file_path)
 
         df.to_csv(file_path)
-
-        print(slide_number)
-
         survey_count += 1
-
-        print(selected_numbers)
         pairs.remove(slide_number)
-        print("Pairs after removal:", pairs)
 
-order_numebr_path = f"./Data/Advertisement/order/{participant_id}-order.txt"
+order_numebr_path = f"./data/display_order/participant_{participant_id}_order.txt"
 directory2 = os.path.dirname(order_numebr_path)
 
 if not os.path.exists(directory2):
